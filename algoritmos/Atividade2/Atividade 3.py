52ae4bff40d8b4295ef50e8810d86347d17ad676
unknown <leonardo.loliveira@rede.ulbra.br>
2022-09-13 20:47:57 -0300

    Primeira mudanca

--- algoritmos/Atividade2/Atividade 3.py
+++ algoritmos/Atividade2/Atividade 3.py
@@ -3,3 +3,6 @@ resultado_1=int(numero_um-1)
 resultado_2=int(numero_um+1)
 print("O antecessor do número é: " +str(resultado_1))
 print("O sucessor do número é: " +str(resultado_2))
+
+
+#primeira mudannca
